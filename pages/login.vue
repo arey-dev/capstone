514c76dbefd5f7e2e2994a543c7c2cee6c6b15cc
ra borja <raborja.dev@gmail.com>
2024-03-05 01:57:43 +0800

    add login function

--- pages/login.vue
+++ pages/login.vue
@@ -3,11 +3,34 @@ definePageMeta({
   layout: 'auth',
 })
 
-const loginForm = reactive({ username: '', pass: '' })
+const client = useSupabaseClient()
+const router = useRouter()
 
-watch(loginForm, () => {
-  console.log(loginForm.value)
+const loginForm = ref({
+  email: '',
+  password: '',
+  loginError: '',
 })
+
+const signIn = async () => {
+  try {
+    const { error } = await client.auth.signInWithPassword({
+      email: loginForm.value.email,
+      password: loginForm.value.password,
+    })
+
+    if (error) throw error
+
+    router.push('/')
+  } catch (error) {
+    if (error.status == 400) {
+      loginForm.value.loginError = error.message
+    }
+
+    // console.log for now the other errors that might error
+    console.log(error)
+  }
+}
 </script>
 
 <template>
@@ -17,37 +40,47 @@ watch(loginForm, () => {
     >
       <h1 class="mb-6 text-2xl">Welcome Back</h1>
 
-      <IconField icon-position="left" class="mb-4">
-        <InputIcon><i class="pi pi-user"></i></InputIcon>
-        <InputText
-          id="username"
-          v-model="loginForm.username"
-          class="w-full"
-          placeholder="username"
-        />
-      </IconField>
+      <div v-if="loginForm.loginError">
+        {{ loginForm.loginError }}
+      </div>
+      <form
+        class="mb-6 flex flex-col gap-4"
+        @submit.prevent="signIn"
+      >
+        <IconField icon-position="left">
+          <InputIcon><i class="pi pi-user"></i></InputIcon>
+          <InputText
+            id="username"
+            v-model="loginForm.email"
+            class="w-full"
+            placeholder="username"
+          />
+        </IconField>
 
-      <IconField icon-position="left" class="mb-6">
-        <InputIcon><i class="pi pi-lock"></i></InputIcon>
-        <InputText
-          id="password"
-          v-model="loginForm.pass"
-          type="password"
-          placeholder="password"
-          class="w-full"
-        />
-      </IconField>
+        <IconField icon-position="left">
+          <InputIcon><i class="pi pi-lock"></i></InputIcon>
+          <InputText
+            id="password"
+            v-model="loginForm.password"
+            type="password"
+            placeholder="password"
+            class="w-full"
+          />
+        </IconField>
 
-      <Button class="mb-6 py-2.5">Login</Button>
+        <Button
+          type="submit"
+          class="py-2.5"
+          label="Login"
+        />
+      </form>
 
       <NuxtLink
-        to="/about"
         class="text-surface-600 mb-1 text-center text-sm underline"
       >
         Forgot your password?
       </NuxtLink>
       <NuxtLink
-        to="/about"
         class="text-surface-600 text-center text-sm underline"
         >Don't have an account? Sign up</NuxtLink
       >
