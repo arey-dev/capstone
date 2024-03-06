33f387888e02c6bb9b223213a758a1193def4b6c
ra borja <raborja.dev@gmail.com>
2024-03-06 11:04:38 +0800

    refacctor

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -2,6 +2,10 @@
   <div
     class="flex min-h-screen w-full max-w-md items-center justify-center p-4"
   >
-    <slot />
+    <div
+      class="flex w-full flex-col rounded-lg border px-4 py-6 font-sans md:px-10"
+    >
+      <slot />
+    </div>
   </div>
 </template>
