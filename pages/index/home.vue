642405b7b23deaa7a79e3e80f1bc0ff31860b982
ra borja <raborja.dev@gmail.com>
2024-03-12 01:52:14 +0800

    add profile card component

---
+++ pages/index/home.vue
@@ -0,0 +1,18 @@
+<script setup>
+const user = useSupabaseUser()
+</script>
+
+<template>
+  <div>
+    <div class="max-w-72">
+      <ProfileCard
+        name="John Doe"
+        :email="user?.email"
+        :followers="1022"
+        :following="2342"
+      />
+    </div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
